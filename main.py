ca1512625500be77895597b188594c35121b0c7a
pikaversoyt-stack <pikaverso.yt@gmail.com>
2026-09-09 18:19:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import firebase_db
 app = Flask(__name__)
 
 # Clave de API de Groq configurada directamente
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "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"
 
 
 @app.route("/")
